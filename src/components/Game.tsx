e983f9b8744c97d7380925704b00587a792db2e3
Jarvis <823867852@qq.com>
2022-04-20 16:56:49 +0800

    ✨ 添加对storage的监听防止对其手动修改；修改hp为负值的处理改为支付医药费恢复hp

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -10,6 +10,13 @@ import {
   Tabs,
   Text,
   useToast,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  useDisclosure
 } from '@chakra-ui/react';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
@@ -28,6 +35,9 @@ export const Game: React.VFC = () => {
   const { data: user, refetch } = useQuery('user', () => {
     return getUser();
   });
+  const [medicalGold,setMedicalGold] = useState(0)
+  const cancelRef = React.useRef()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const clearTimer = useCallback(() => {
     if (timer) {
@@ -40,7 +50,10 @@ export const Game: React.VFC = () => {
         duration: 1500,
       });
     }
-  }, [timer, toast]);
+    if (isRest && timer) {
+        setIsRest(false);
+      }
+  }, [timer, toast, isRest]);
 
   const setTimer = useCallback(
     (func: any, timeout = 3000) => {
@@ -94,6 +107,28 @@ export const Game: React.VFC = () => {
     }, 5000);
   };
 
+  const handlePay = () => {
+    if(user && user.gold > medicalGold) {
+        user.gold -= medicalGold;
+        user.hp = 1;
+        toast({
+            title: '支付成功',
+            description: '员工生命体征恢复，请注意休息',
+            duration: 2000,
+            status: 'success',
+          });
+    }else {
+        toast({
+            title: '支付失败',
+            description: '余额不足',
+            duration: 2000,
+            status: 'warning',
+        });
+        return
+    }
+    onClose()
+  }
+
   useEffect(() => {
     if (user && user.hp === MAX_HP && isRest && !!timer) {
       clearTimer();
@@ -102,10 +137,10 @@ export const Game: React.VFC = () => {
   }, [clearTimer, isRest, timer, user]);
 
   useEffect(() => {
-    if (user && user.hp <= 0 && timer && !isRest) {
+    if (user && user.hp === 0 && timer && !isRest) {
       clearTimer();
       toast({
-        title: '健康值小于0',
+        title: '员工已经休克',
         description: '已停止打工，请注意休息',
         duration: null,
         status: 'error',
@@ -114,6 +149,14 @@ export const Game: React.VFC = () => {
       });
     }
   }, [clearTimer, isRest, timer, toast, user]);
+
+  useEffect(() => {
+    if (user && user.hp < 0) {
+        setMedicalGold(10 + Math.ceil(user.exp / 100) * (0 - user.hp))
+        clearTimer();
+        onOpen()
+    }
+  }, [clearTimer, user]);
 
   useEffect(() => {
     const now = Date.now();
@@ -126,6 +169,13 @@ export const Game: React.VFC = () => {
   if (!user) {
     return null;
   }
+
+  window.addEventListener('storage', function(e) {
+    if(e.key === SAVE_USER_KEY) {
+      const old = e.oldValue || '';
+      localStorage.setItem(SAVE_USER_KEY, old)
+    }
+  });
 
   return (
     <Tabs
@@ -173,7 +223,7 @@ export const Game: React.VFC = () => {
             </Button>
             <Button
               colorScheme="green"
-              disabled={user.hp >= MAX_HP}
+              disabled={user.hp >= MAX_HP || user.hp < 0}
               onClick={onToggleRest}
               isLoading={isRest}
               loadingText="取消休息"
@@ -206,6 +256,32 @@ export const Game: React.VFC = () => {
         <Tab>工作</Tab>
         {/* <Tab>商店</Tab> */}
       </TabList>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                员工已住进ICU
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+            请支付医疗费用挽救员工生命
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                放弃治疗
+              </Button>
+              <Button colorScheme='red' onClick={handlePay} ml={3}>
+                支付 ￥{medicalGold}
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </Tabs>
   );
 };
